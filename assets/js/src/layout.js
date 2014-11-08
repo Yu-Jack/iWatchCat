64ae308909febdb3499ce765a6602885b279313d
Jack <jk82421@gmail.com>
2014-11-08 13:08:15 +0000

    Add react src

---
+++ assets/js/src/layout.js
@@ -0,0 +1,24 @@
+var All = React.createClass({
+    handleClick: function(event,reactid){
+        var $currentImg = $(this.getDOMNode()).find('.image[data-reactid="' + reactid + '"]');
+
+        $('#galler-colorbox').find('img').attr('src', $currentImg.attr('href'));
+
+        $.colorbox({
+            html: $('#galler-colorbox').html()
+        });
+    },
+    render: function(){
+        var _self = this;
+        var image = this.props.url.map(function(m,index){
+            var divStyle = {
+                background: 'url(' + m +') center center no-repeat',
+                backgroundSize: 'cover'
+            };
+            var divClass = "col-md-4 col-xs-12 image";
+
+            return <div key = { index } onClick = { _self.handleClick } className = {divClass} style = {divStyle} href = { m }></div>
+        });
+        return ( <div className="row">{image}</div>);
+    }
+});
